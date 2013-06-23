e897dec491982cda0ac8c6fdad0788929bda158a
Bryce Hammond <bryce.hammond@imulus.com>
2013-06-22 18:40:41 -0600

    Turn off idle timer

--- BirthCoach/BCAppDelegate.m
+++ BirthCoach/BCAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:@"BirthCoach.sqlite"];
+    [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
     return YES;
 }
 							
