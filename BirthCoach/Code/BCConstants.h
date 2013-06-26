a1e77868d17f27e2d87c792b3bc08eb0efdf9009
Bryce Hammond <bryce.hammond@imulus.com>
2013-06-26 01:14:06 -0600

    Add colors and fonts to project

--- BirthCoach/Code/BCConstants.h
+++ BirthCoach/Code/BCConstants.h
@@ -11,6 +11,16 @@
 #define kForegroundingNotification @"ForegroundingNotification"
 #define kBackgroundingNotification @"BackgroundingNotification"
 
+//Colors
+
+#define kDarkBlueColor @"0D243E" //R: 13 G: 36 B: 62
+#define kDarkGreenColor @"126F89" //R: 18 G: 111 B: 137
+#define kMidGreenColor  @"5FB1B9" //R: 95 G: 177 B: 185
+#define kLightGreenColor    @"BFE0E2" //R: 191 G: 224 B: 226
+#define kLightOrangeColor   @"F9E6D4" //R: 249 G: 230 B: 212
+#define kMidOrangeColor @"F2C497" //R: 242 G: 196 B: 151
+#define kDarkOrangeColor    @"DE9247" //R: 222 G: 146 B: 71
+
 @interface BCConstants : NSObject
 
 @end
