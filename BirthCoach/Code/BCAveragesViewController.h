03c12e0cd654b0b43c42eccb697ed45f5ba55de8
Bryce Hammond <bryce.hammond@imulus.com>
2013-06-29 12:37:36 -0600

    Add averages controller as a subcontroller of the root controller

---
+++ BirthCoach/Code/BCAveragesViewController.h
@@ -0,0 +1,15 @@
+//
+//  BCAveragesViewController.h
+//  BirthCoach
+//
+//  Created by Bryce Hammond on 6/29/13.
+//  Copyright (c) 2013 Fluidvision Design, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCAveragesViewController : UIViewController
+
+- (void)updateAverages;
+
+@end
---
+++ BirthCoach/Code/BCAveragesViewController.m
@@ -0,0 +1,75 @@
+//
+//  BCAveragesViewController.m
+//  BirthCoach
+//
+//  Created by Bryce Hammond on 6/29/13.
+//  Copyright (c) 2013 Fluidvision Design, LLC. All rights reserved.
+//
+
+#import "BCAveragesViewController.h"
+
+@interface BCAveragesViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *averagesScrollView;
+@property (weak, nonatomic) IBOutlet UILabel *averageLabel;
+
+
+//15 Minute
+@property (weak, nonatomic) IBOutlet UILabel *fifteenMinuteDuration;
+@property (weak, nonatomic) IBOutlet UILabel *fifteenMinuteIntensity;
+@property (weak, nonatomic) IBOutlet UILabel *fifteenMinuteFrequency;
+
+//30 Minute
+@property (weak, nonatomic) IBOutlet UILabel *thirtyMinuteDuration;
+@property (weak, nonatomic) IBOutlet UILabel *thirtyMinuteIntensity;
+@property (weak, nonatomic) IBOutlet UILabel *thirtyMinuteFrequency;
+
+//1 Hour
+@property (weak, nonatomic) IBOutlet UILabel *oneHourDuration;
+@property (weak, nonatomic) IBOutlet UILabel *oneHourIntensity;
+@property (weak, nonatomic) IBOutlet UILabel *oneHourFrequency;
+
+@property (nonatomic, strong) NSArray *dataLabels;
+
+@end
+
+@implementation BCAveragesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.averagesScrollView.contentSize = CGSizeMake(self.averagesScrollView.frame.size.width * 3, self.averagesScrollView.frame.size.height);
+    self.averageLabel.font = [UIFont fontWithName:@"SourceSansPro-Black" size:self.averageLabel.font.pointSize];
+    self.dataLabels = @[self.fifteenMinuteDuration, self.fifteenMinuteIntensity, self.fifteenMinuteFrequency,
+                        self.thirtyMinuteDuration, self.thirtyMinuteIntensity, self.thirtyMinuteFrequency,
+                        self.oneHourDuration, self.oneHourIntensity, self.oneHourFrequency];
+    
+    for(UILabel *dataLabel in self.dataLabels)
+    {
+        dataLabel.font = [UIFont fontWithName:@"OpenSans" size:dataLabel.font.pointSize];
+    }
+    
+    for(UIView *subview in self.averagesScrollView.subviews)
+    {
+        if([subview isKindOfClass:[UILabel class]] && NO == [self.dataLabels containsObject:subview])
+        {
+            UILabel *titleLabel = (UILabel *)subview;
+            titleLabel.font = [UIFont fontWithName:@"SourceSansPro-Black" size:titleLabel.font.pointSize];
+        }
+    }
+}
+
+- (void)updateAverages
+{
+    
+}
+
+@end
