cf22cd39566d3683840ac6c25dc88df8df11eb5c
Bryce Hammond <bryce.hammond@imulus.com>
2013-08-04 16:39:20 -0600

    Add an audio reminder player class to take care of actual playback

---
+++ BirthCoach/Code/BCAudioReminderPlayer.h
@@ -0,0 +1,21 @@
+//
+//  BCAudioReminderPlayer.h
+//  BirthCoach
+//
+//  Created by Bryce Hammond on 8/4/13.
+//  Copyright (c) 2013 Fluidvision Design, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@class BCAudioReminder;
+
+@interface BCAudioReminderPlayer : NSObject
+
+@property (nonatomic, strong) BCAudioReminder *reminder;
+
+- (void)play;
+- (void)stop;
+
+@end
---
+++ BirthCoach/Code/BCAudioReminderPlayer.m
@@ -0,0 +1,41 @@
+//
+//  BCAudioReminderPlayer.m
+//  BirthCoach
+//
+//  Created by Bryce Hammond on 8/4/13.
+//  Copyright (c) 2013 Fluidvision Design, LLC. All rights reserved.
+//
+
+#import "BCAudioReminderPlayer.h"
+#import "BCAudioReminder.h"
+
+@interface BCAudioReminderPlayer ()
+
+@property (nonatomic, strong) AVAudioPlayer *player;
+
+@end
+
+@implementation BCAudioReminderPlayer
+
+@synthesize reminder = _reminder;
+
+- (void)setReminder:(BCAudioReminder *)reminder
+{
+    _reminder = reminder;
+    [self.player stop];
+    self.player = [[AVAudioPlayer alloc] initWithData:reminder.audioData error:nil];
+    [self.player prepareToPlay];
+
+}
+
+- (void)play
+{
+    [self.player play];
+}
+
+- (void)stop
+{
+    [self.player stop];
+}
+
+@end
